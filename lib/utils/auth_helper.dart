cab5fcdc853717b326ee2a4cf66c774d8753d8fa
omnitechphilippines <omnitechphilippines@users.noreply.github.com>
2025-07-02 09:23:45 +0800

    Remove unnecessary files.

--- lib/utils/auth_helper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
-bool isUserLoggedIn() {
-  final Box<dynamic> box = Hive.box('auth');
-  final String? token = box.get('token');
-
-  return token != null;
-}
